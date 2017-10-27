28dcb1a0ad4fe78fb06dcd2da861d9e5a30977d5
Arnaud Couchet <aco@softfluent.com>
2017-10-27 17:09:32 +0200

    Better onclose statement

--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ const {app, BrowserWindow} = electron
 let mainWindow
 app.on('ready', _ => {
     mainWindow = new BrowserWindow({
-        height: 400,
-        width: 400
+        height: 725,
+        width: 1200,
+        resizable: false
     })
-})
 
-app.on('close', _ => {
-    mainWindow = null
+    mainWindow.loadURL(`file://${__dirname}/capture.html`)
+
+    mainWindow.webContents.openDevTools()
+
+    mainWindow.on('close', _ => {
+        mainWindow = null
+    })
 })
